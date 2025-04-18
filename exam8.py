058f4b1f96fc4604eac4f0ec897ec446f3790b37
PRPRSPR <s.subin.0208@gmail.com>
2025-04-18 18:10:46 +0900

    250418 - 파이썬 연습문제

---
+++ exam8.py
@@ -0,0 +1,71 @@
+# 1. 문자열 뒤집기 함수
+# def reverse_string(s):
+# 	pass
+
+# reverse_string 함수 완성시키기
+
+def reverse_string(s):
+    return s[::-1]
+
+print(reverse_string("asdfgh jkl;'"))
+
+
+
+# 2. 중복 없는 로또 번호 생성기
+# 1부터 45까지 숫자 중 중복 없이 6개를 랜덤으로 골라 리스트로 반환하는 함수
+# sample() 함수 사용 금지
+
+# def lotto():
+# 	list = []
+# 	return list
+
+# lotto 함수 완성시키기
+
+import random
+
+def lotto():
+	list = []
+    
+	return list
+
+
+# 3. 단어별 첫 글자 대문자로 바꾸기 (타이틀 케이스)
+# "this is a title" → "This Is A Title"
+# str.title() 함수 사용 금지
+
+# def to_title_case(text):
+# 	pass
+
+# to_title_case 함수 완성시키기
+
+def to_title_case(text):
+	
+    pass
+
+
+
+# 4. 리스트 압축 함수 만들기
+# 연속된 동일 요소들을 하나의 요소로 압축
+# 예: [1, 1, 2, 2, 2, 3, 1, 1] → [1, 2, 3, 1]
+
+# def compress_list(list):
+# 	pass
+	
+# compress_list 함수 완성시키기
+
+def compress_list(list):
+	pass
+
+
+# 5. 자리수 분리 및 합계 구하기
+# 정수 하나를 입력받아, 각 자리의 숫자를 분리해서 리스트로 반환하고,
+# 그 자리수들의 합도 같이 반환하는 함수
+
+# func(1234) → ([1, 2, 3, 4], 10)
+# func(908) → ([9, 0, 8], 17)
+
+# def split_sum(num) : 
+# 	pass
+
+def split_sum(num) : 
+	pass
